7f5a55c5211bf4a483af53fde78436f1e0c7aa69
MiraArndt <mira.arndt@tu-dortmund.de>
2019-11-29 20:04:02 +0100

    Plots erstellen

---
+++ plot3.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+x=np.genfromtxt("Messwerte.csv", delimiter=",",unpack=True,usecols=0)
+y=np.genfromtxt("Messwerte.csv", delimiter=",",unpack=True,usecols=1)
+H=595/(2*np.pi*0.13)*x
+Hrund=["%.0f" %elem for elem in H]
+xrund=["%.0f" %elem for elem in x]
+yrund=["%.0f" %elem for elem in y]
+
+with open("tabelle1.csv", "w") as f:
+    writer= csv.writer(f)
+    writer.writerows(zip(xrund,yrund,Hrund))
+
+plt.plot(H[9:49],y[9:49],'r.')
+plt.plot(H[0:8],y[0:8],'.')
+plt.grid(axis='both')
+plt.xlabel(r'$H \,/\, \mathrm{A\cdot m^{-1}}$')
+plt.ylabel(r'$B \,/\, \mathrm{mT}$')
+
+
+plt.savefig('build/plot3.pdf')
+
