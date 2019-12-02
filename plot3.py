318f25762e4e9993e995237d16edccb6e175462c
MiraArndt <mira.arndt@tu-dortmund.de>
2019-12-03 00:15:55 +0100

    Letzte Änderungen für heute

--- plot3.py
+++ plot3.py
@@ -13,10 +13,14 @@ with open("tabelle1.csv", "w") as f:
     writer= csv.writer(f)
     writer.writerows(zip(xrund,yrund,Hrund))
 
+a=[-728.43,0]
+b=[-75,131]
+
 plt.plot(H[0:8],y[0:8],'r.',label='Neukurve')
 plt.plot(H[9:29],y[9:29],'b.',label='Verringern des Magnetfeldes')
 plt.plot(H[29:49],y[29:49],'c.', label='Erhöhen des Magnetfeldes')
-plt.legend()
+plt.plot(a,b,'k-',label='Lineare Ausgleichsgerade')
+plt.legend(loc='best',prop={'size':8})
 
 
 
